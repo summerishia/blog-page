71db442e952af946d40087b77ded24f024af3b58
Su Wang <shrekwang1985@gmail.com>
2022-07-17 10:45:57 -0500

    add deblur menu collapse.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(function () {  // same as document.addEventListener("DOMContentLoaded").
 
 	// same as document.querySelector("#navbarToggle").addEventListener("blur").
+	// "select" syntax: $(some-css-handle).
 	$("#navbarToggle").blur(function (event) {
 		var screenWidth = window.innerWidth;
 		if (screenWidth < 768) {
